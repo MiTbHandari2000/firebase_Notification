a76a12293d9f0274d3f08eda7625774cdff94227
meet.bhandari <meet.bhandari@1R-74>
2022-02-18 15:16:23 +0530

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Gms.Extensions;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using AndroidX.AppCompat.App;
+using Firebase.Messaging;
+using System;
+using System.Threading.Tasks;
+
+namespace firebase_Notification
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.notification_activity);
+            _ = GetFirbaseToken();
+        }
+
+        private async Task GetFirbaseToken()
+        {
+            try
+            {
+                var token = await FirebaseMessaging.Instance.GetToken();
+            }
+            catch (Exception ex)
+            {
+                Log.Debug("klasfjsadjkbjasdbn", ex.Message);
+            }
+        }
+    }
+}
---
+++ NotificationMessagingService.cs
@@ -0,0 +1,93 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
+using Firebase.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public delegate void NotificationReceived(bool isclearNotificationFromBanner);
+namespace firebase_Notification
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class NotificationMessagingService : FirebaseMessagingService
+    {
+        private const string TAG = "NotificationMessagingService";
+        private string _body;
+        public readonly string ChannelId = "my_notification_channel";
+
+        public override void OnNewToken(string token)
+        {
+            base.OnNewToken(token);
+            Log.Debug(TAG, "onNewToken: " + token);
+        }
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            try
+            {
+
+                Log.Debug(TAG, "From:" + message.From);
+
+
+                var notification = message.GetNotification();
+                if (notification != null && notification.Body != null)
+                {
+                    _body = notification.Body;
+                }
+                else
+                {
+                    _body = "Push Notification";
+                }
+                DisplayNotification(notification.Title, _body, message.Data);
+            }
+            catch (Exception ex)
+            {
+                Log.Info(TAG, ex.StackTrace);
+            }
+        }
+        void DisplayNotification(string title, string messageBody, IDictionary<string, string> data)
+        {
+            try
+            {
+                var intent = new Intent(this, typeof(NotificationActivity));
+                var pendingIntent = PendingIntent.GetActivity(this,
+                                                              101,
+                                                              intent,
+                                               PendingIntentFlags.UpdateCurrent);
+                var notificationManager = NotificationManagerCompat.From(this);
+                if (Android.OS.Build.VERSION.SdkInt >= BuildVersionCodes.O)
+                {
+
+                    NotificationChannel notificationChannel =
+                        new NotificationChannel(ChannelId, Resources.GetString(Resource.String.app_name), NotificationImportance.High);
+                    notificationManager.CreateNotificationChannel(notificationChannel);
+                }
+                var notificationBuilder = new NotificationCompat.Builder(this, ChannelId)
+                                            .SetSmallIcon(Resource.Drawable.saitama)
+                                            .SetColor(ContextCompat.GetColor(ApplicationContext, Resource.Color.colorAccent))
+                                            .SetColorized(true)
+                                            .SetContentTitle(title)
+                                            .SetContentText(messageBody)
+                                            .SetAutoCancel(true)
+                                            .SetDefaults((int)NotificationDefaults.All)
+                                           .SetContentIntent(pendingIntent);
+                notificationManager.Notify(101, notificationBuilder.Build());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+
+
